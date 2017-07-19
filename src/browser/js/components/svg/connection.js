df6030e3153c05079ed9461e706c08c35b4e8db7
Filip Vitas <felix557700@hotmail.com>
2017-07-19 02:56:21 +0200

    consider curve connection

--- src/browser/js/components/svg/connection.js
+++ src/browser/js/components/svg/connection.js
@@ -6,13 +6,12 @@ class Connection extends Component {
         let translate = `translate(${x1}, ${y1})`
         let storkeWidth = props.zoom
 
+        // let d = `M${x1},${y1} C${x1 + 100},${y1} ${x2 - 100},${y2} ${x2},${y2}`
+        // <path id='curve' d={d} />
+
         return (
             <g transform={translate} >
-                {/* <text x={(x2 - x1) / 3} y={(y2 - y1) / 3}>0</text> */}
-
                 <line class='node-connection' x1='0' y1='0' x2={x2 - x1} y2={y2 - y1} stroke-width={storkeWidth} stroke='black' onClick={() => { console.log('click on line') }} />
-
-                {/* <text x={(x2 - x1) * 2 / 3} y={(y2 - y1) * 2 / 3}>0</text> */}
             </g>
         )
     }
