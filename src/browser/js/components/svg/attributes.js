96e1d1156c2852b9e8de2d7b14f675ab6e11c0ea
Filip Vitas <felix557700@hotmail.com>
2017-08-27 12:17:44 +0200

    modify attribute layout with delete button/icon

--- src/browser/js/components/svg/attributes.js
+++ src/browser/js/components/svg/attributes.js
@@ -26,12 +26,15 @@ class Attributes extends Component {
                 {
                     props.attributes.map(attribute => (
                         <div class='node-attribute'>
-                            <span class={attribute.isPrimary ? 'primary-key' : ''}>{attribute.name}</span>
-                            <svg class='delete-attribute' onClick={this.onAttributeDelete.bind(this, attribute.name)}>
-                                <circle cx='10' cy='10' r='9' fill='white' stroke='black' />
-                                <polyline points='5 15, 15 5' stroke='black' />
-                                <polyline points='15 15, 5 5' stroke='black' />
-                            </svg>
+                            <div class='node-attribute-wrapper'>
+                                <span class={attribute.isPrimary ? 'primary-key' : ''}>{attribute.name}</span>
+
+                                <svg class='delete-attribute' onClick={this.onAttributeDelete.bind(this, attribute.name)}>
+                                    <circle cx='10' cy='10' r='9' fill='white' stroke='black' />
+                                    <polyline points='5 15, 15 5' stroke='black' />
+                                    <polyline points='15 15, 5 5' stroke='black' />
+                                </svg>
+                            </div>
                         </div>
                     ))
                 }
